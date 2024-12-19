096a369f8de4350d00091114544f04bcd5114b3a
noritoshi-kumagai <noritoshi-kumagai@bm-sms.co.jp>
2024-12-19 18:51:06 +0900

    onceを削除
    afterEachでresetHandlers()されていると、onceは不要.テストごとにリセットされるため

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,20 +3,16 @@ import { app } from "./index";
 import { server } from "./mock/node";
 import { HttpResponse, http } from "msw";
 
+test("fetch app with network error", async () => {
+	server.use(
+		http.get("https://example.com/hello", () => {
+			return HttpResponse.error();
+		}),
+	);
+	await expect(app()).rejects.toThrow("Network error");
+});
+
 test("fetch app", async () => {
 	const res = await app();
 	expect(res).toStrictEqual({ message: "Hello, World!" });
-});
-
-test("fetch app with network error", async () => {
-	server.use(
-		http.get(
-			"https://example.com/hello",
-			() => {
-				return HttpResponse.error();
-			},
-			{ once: true },
-		),
-	);
-	await expect(app()).rejects.toThrow("Network error");
 });
